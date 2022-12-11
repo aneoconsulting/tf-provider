bb9b2caa2c1367bc3fa7d4291e0cbcd07a500346
lemaitre <lemaitre@users.noreply.github.com>
2022-12-11 21:54:40 +0100

    IO over gRPC

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 
 use std::pin::Pin;
+use std::fmt::Write;
 
 use futures::{Stream, StreamExt};
 use tokio::sync::broadcast;
@@ -21,8 +22,10 @@ pub mod tfplugin6 {
     tonic::include_proto!("tfplugin6");
 }
 
-#[derive(Debug, Default)]
-pub struct MyGrpcBroker {}
+#[derive(Debug)]
+pub struct MyGrpcBroker {
+    pub io: GrpcIo,
+}
 
 #[tonic::async_trait]
 impl GrpcBroker for MyGrpcBroker {
@@ -33,9 +36,11 @@ impl GrpcBroker for MyGrpcBroker {
     ) -> Result<Response<Self::StartStreamStream>, Status> {
         let mut stream = request.into_inner();
 
+        let io = self.io.clone();
         let output = async_stream::try_stream! {
             while let Some(conn_info) = stream.next().await {
                 let conn_info = conn_info?;
+                write!(io.stdout(), "relay {}\n", conn_info).unwrap();
                 yield conn_info.clone();
             }
         };
@@ -44,8 +49,16 @@ impl GrpcBroker for MyGrpcBroker {
     }
 }
 
-#[derive(Debug, Default)]
-pub struct MyGrpcController {}
+impl core::fmt::Display for ConnInfo {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "ConnInfo{{service_id: {}, network: {}, address: {} }}", self.service_id, self.network, self.address)
+    }
+}
+
+#[derive(Debug)]
+pub struct MyGrpcController {
+    pub io: GrpcIo,
+}
 
 #[tonic::async_trait]
 impl GrpcController for MyGrpcController {
@@ -53,6 +66,8 @@ impl GrpcController for MyGrpcController {
         &self,
         _request: tonic::Request<Empty>,
     ) -> Result<tonic::Response<Empty>, tonic::Status> {
+        write!(self.io.stderr(), "shutdown requested\n").unwrap();
+
         Ok(Response::new(Empty{}))
     }
 }
@@ -72,8 +87,15 @@ impl GrpcStdio for MyGrpcStdio {
         let mut rx = self.tx.subscribe();
 
         let output = async_stream::try_stream! {
-            while let Ok(iodata) = rx.recv().await {
-                yield iodata.clone();
+            loop {
+                let iodata = rx.recv().await;
+                if let Ok(iodata) = iodata {
+                    yield iodata.clone();
+                } else if let Err(broadcast::error::RecvError::Lagged(n)) = iodata {
+                    eprintln!("IO over grpc lags behind by {n} messages!");
+                } else {
+                    break;
+                }
             }
         };
 
@@ -81,14 +103,48 @@ impl GrpcStdio for MyGrpcStdio {
     }
 }
 
+#[derive(Debug,Clone)]
+pub struct GrpcIoStream<'a> {
+    pub tx: &'a broadcast::Sender<StdioData>,
+    pub channel: i32,
+}
+
+impl std::fmt::Write for GrpcIoStream<'_> {
+    fn write_str(&mut self, s: &str) -> Result<(), std::fmt::Error> {
+        match self.tx.send(StdioData{channel: self.channel, data: s.as_bytes().to_vec()}) {
+            Ok(_) => Ok(()),
+            Err(_) => Ok(()),
+        }
+    }
+}
+
+#[derive(Debug,Clone)]
+pub struct GrpcIo {
+    pub tx: broadcast::Sender<StdioData>,
+}
+
+impl GrpcIo {
+    fn stdout(&self) -> GrpcIoStream {
+        GrpcIoStream{tx: &self.tx, channel: 1}
+    }
+    fn stderr(&self) -> GrpcIoStream {
+        GrpcIoStream{tx: &self.tx, channel: 2}
+    }
+}
+
+
+
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let grpc_broker = MyGrpcBroker::default();
-    let grpc_controller = MyGrpcController::default();
     let (tx, _) = broadcast::channel(10);
-    let grpc_stdio = MyGrpcStdio{tx};
+    let grpc_io = GrpcIo{tx: tx.clone()};
+
+    let grpc_broker = MyGrpcBroker{io: grpc_io.clone()};
+    let grpc_controller = MyGrpcController{io:grpc_io};
+    let grpc_stdio = MyGrpcStdio{tx: tx};
 
     Server::builder()
         .add_service(GrpcBrokerServer::new(grpc_broker))
