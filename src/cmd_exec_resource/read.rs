eb287c904b9a9a2ee077e03a987484d1e7c0246d
lemaitre <lemaitre@users.noreply.github.com>
2023-03-25 23:05:48 +0100

    Parallel reads

--- src/cmd_exec_resource/read.rs
+++ src/cmd_exec_resource/read.rs
@@ -2,6 +2,7 @@ use std::borrow::Cow;
 
 use async_trait::async_trait;
 
+use futures::{stream, StreamExt};
 use tf_provider::{AttributePath, Diagnostics, Value};
 
 use crate::connection::Connection;
@@ -39,49 +40,61 @@ where
         let reads_default = Default::default();
         let reads = self.read.as_ref().unwrap_or(&reads_default);
 
-        for (name, value) in state {
+        let mut read_tasks = Vec::new();
+
+        for (name, value) in state.iter_mut() {
             if !value.is_unknown() {
                 continue;
             }
             if let Some(Value::Value(read)) = reads.get(name) {
-                let attr_path = AttributePath::new("read")
-                    .key(name.to_string())
-                    .attribute("cmd");
                 let cmd = read.cmd.as_str();
 
-                match connect
-                    .execute(connection, cmd, with_env(&env, &read.env))
-                    .await
-                {
-                    Ok(res) => {
-                        if res.status == 0 {
-                            if res.stderr.len() > 0 {
-                                diags.warning(
-                                    "`read` succeeded but stderr was not empty",
-                                    res.stderr,
-                                    attr_path,
-                                );
-                            }
-
-                            *value = Value::Value(res.stdout.into());
-                        } else {
-                            diags.warning(
-                                format!("`read` failed with status code: {}", res.status),
-                                res.stderr,
-                                attr_path,
-                            );
-                        }
-                    }
-                    Err(err) => {
-                        diags.warning("Failed to read resource state", err.to_string(), attr_path);
-                    }
-                };
+                read_tasks.push(async move {
+                    let result = connect
+                        .execute(connection, cmd, with_env(&env, &read.env))
+                        .await;
+                    (name, (value), result)
+                });
             } else {
                 diags.error(
                     "Unknown output has no `read` block associated",
                     format!("The output `state.{name}` is unknown, and there is no known `read[\"{name}\"]` block to give it a value."),
                     AttributePath::new("state").key(name.to_string())
                 );
+            }
+        }
+
+        for (name, value, result) in stream::iter(read_tasks.into_iter())
+            .buffer_unordered(6)
+            .collect::<Vec<_>>()
+            .await
+        {
+            let attr_path = AttributePath::new("read")
+                .key(name.to_string())
+                .attribute("cmd");
+            match result {
+                Ok(res) => {
+                    if res.status == 0 {
+                        if res.stderr.len() > 0 {
+                            diags.warning(
+                                "`read` succeeded but stderr was not empty",
+                                res.stderr,
+                                attr_path,
+                            );
+                        }
+
+                        *value = Value::Value(res.stdout.into());
+                    } else {
+                        diags.warning(
+                            format!("`read` failed with status code: {}", res.status),
+                            res.stderr,
+                            attr_path,
+                        );
+                    }
+                }
+                Err(err) => {
+                    diags.warning("Failed to read resource state", err.to_string(), attr_path);
+                }
             }
         }
 
